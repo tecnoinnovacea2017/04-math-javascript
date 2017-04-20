a8e145ce1182887b6c27b99256572860c37addc7
Mauricio Collazos <ma0@192.168.1.50>
2017-04-20 09:46:04 -0500

    added structure for math package

---
+++ ejercicios.js
@@ -0,0 +1,19 @@
+// Esta es una lista de ejercicios que debes completar
+
+// 1. Realiza una función que sume números consecutivos hasta un tope
+function sumaNumerosConsecutivos(tope)
+{
+  
+}
+
+// 2. Realiza una función retorne todos los números pares hasta un tope
+function pares(tope)
+{
+
+}
+
+// 3. Realiza una función obtenga el residuo de una función usando restas sucesivas
+function residuo(dividendo, divisor)
+{
+
+}
